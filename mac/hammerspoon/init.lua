99091e40a926e9b2b04e801c5be6f1662118ada1
Oleg Utkin <oleg.utkin@uber.com>
2017-06-20 23:32:58 -0700

    Misc updates

--- mac/hammerspoon/init.lua
+++ mac/hammerspoon/init.lua
@@ -1,23 +1,24 @@
 prefix = require("prefix")
 utils = require("utils")
+qa = require("menu_bar")
+
 require("window_manager")
 
-
-qa = {}
-qa.menu = {
-  { title = "Work Utils", 
-    menu = {
-      { title = "Mail GenStatus (This Week)", fn = function () workUtil_GenStatus(1) end },
-      { title = "Mail GenStatus (Past Week)", fn = function () workUtil_GenStatus(0) end }
-    }
-  },
-}
-
-qa.bar = hs.menubar.new()
-qa.bar:setTitle("QAct")
-qa.bar:setMenu(qa.menu)
-
-function workUtil_GenStatus(thisWeek)
-  manager = "dsawyer@uber.com"
-  utils.composeMail(manager, "Hello", "<b>Focus:</b> lalalal \n")
-end
+-- qa = {}
+-- qa.menu = {
+--   { title = "Work Utils", 
+--     menu = {
+--       { title = "Mail GenStatus (This Week)", fn = function () workUtil_GenStatus(1) end },
+--       { title = "Mail GenStatus (Past Week)", fn = function () workUtil_GenStatus(0) end }
+--     }
+--   },
+-- }
+--
+-- qa.bar = hs.menubar.new()
+-- qa.bar:setTitle("QAct")
+-- qa.bar:setMenu(qa.menu)
+--
+-- function workUtil_GenStatus(thisWeek)
+--   manager = "dsawyer@uber.com"
+--   utils.composeMail(manager, "Hello", "<b>Focus:</b> lalalal \n")
+-- end
--- mac/hammerspoon/menu_bar.lua
+++ mac/hammerspoon/menu_bar.lua
@@ -1,29 +1,37 @@
+prefix = require("prefix")
+
 qa = {}
 qa.funcs = {}
+qa.bar = hs.menubar.new()
+qa.title = "QQ"
 
-function qa.mkmenu(title, submenu, fn)
+function qa.mkmenu(title, ...)
+  local arg = {...}
+  cmp = arg[1]
+  if cmp == nil then
+    return {title=title}
+  elseif type(cmp) == 'function' then
+    return {title=title, fn = cmp}
+  elseif type(cmp) == 'table' then
+    return {title=title, menu = cmp}
+  end
+end
 
+function qa.update()
+  qa.bar:setTitle(qa.title)
+  qa.bar:setMenu(qa.menu)
 end
 
 qa.menu = {
-  { title = "Work Utils", 
-    menu = {
-      { title = "Mail GenStatus (This Week)", fn = function () workUtil_GenStatus(1) end },
-      { title = "Mail GenStatus (Past Week)", fn = function () workUtil_GenStatus(0) end }
-    }
-  },
+  qa.mkmenu("Quick Actions"),
+  qa.mkmenu("-"),
+  qa.mkmenu("Utils")
 }
+qa.update()
 
-qa.bar = hs.menubar.new()
-qa.bar:setTitle("QAct")
-qa.bar:setMenu(qa.menu)
-
-
-function qa.funcs.workUtil_GenStatus(thisWeek)
-  manager = "dsawyer@uber.com"
-  utils.composeMail(manager, "Hello", "<b>Focus:</b> lalalal \n")
-end
-
-
+prefix.bind({}, 'q', function() 
+  local p = hs.screen.mainScreen():frame().center
+  qa.bar:popupMenu(p)
+end)
 
 return qa
