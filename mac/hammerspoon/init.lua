2408e73a225645d68a0b52be1ef65221421e387a
Oleg Utkin <oleg.utkin@uber.com>
2018-02-15 16:41:21 -0800

    Streamlining my custom WM

--- mac/hammerspoon/init.lua
+++ mac/hammerspoon/init.lua
@@ -13,47 +13,167 @@ local windowMap = {}
 local windowSizeStep = 300
 
 ------------------------------------------------------------------------
---                            Helper Utils                            --
+--                               KeyMap                               --
 ------------------------------------------------------------------------
 
+function init_keymap() -- {{{
+  print("STARTING")
+
+  -- Global Mappings
+
+  prefix.bind({}, '`', hs.toggleConsole)
+  prefix.bind({}, '\\', hs.reload)
+
+  -- Language Manager
+
+  prefix.bind({}, 'n', langSwitch("U.S."))
+  prefix.bind({}, 'm', langSwitch("Russian - Phonetic"))
+
+  -- Grid Window Manager
+
+  -- bindGrid(prefix, "q", {
+  --   "abc",
+  --   "abc",
+  -- })
+
+  bindGrid(prefix, "w", {
+    "aaaabbbbbccc",
+  })
+
+  bindGrid(prefix, "e", {
+    "bbbbbbaaaa",
+    "bbbbbbaaaa",
+    "bbbbbbcccc", 
+  })
+
+  bindGrid(prefix, "r", {
+    "aaaaaabbbb",
+  })
+
+  bindGrid(prefix, "t", {
+    "aaaaaabbbb",
+    "aaaaaabbbb",
+    "aaaaaabbbb",
+    "aaaaaacccc", 
+    "aaaaaacccc", 
+  })
+
+  bindGrid(prefix, "y", {
+    "aaaaaabbbb",
+  })
+
+  -- Window operations
+  
+  hs.hotkey.bind({'ctrl', 'alt'}, "\\", winSelect)
+  
+  prefix.bind({}, "q", winRestoreFrame)
+  prefix.bind({'shift'}, "q", winSaveFrameAll)
+
+  prefix.bind({}, 'h', hs.window.focusWindowWest)
+  prefix.bind({}, 'j', hs.window.focusWindowSouth)
+  prefix.bind({}, 'k', hs.window.focusWindowNorth)
+  prefix.bind({}, 'l', hs.window.focusWindowEast)
+
+  --# Expand a window vertically
+  prefix.bind({}, 'p', resizeFocusedWindow(function(w,s,f)
+    f.y = 0
+    f.h = s:frame()._h
+    return f
+  end))
+
+  -- Window Sizer
+  
+  local winSizeMult = 0.2
+
+  prefix.bind({'shift'}, 'h', resizeFocusedWindow(function(w,s,f)
+      local step = winSizeMult * s:frame()._w
+      f.x = f.x - step
+      f.w = f.w + step
+      return f
+  end))
+
+  prefix.bind({'shift'}, 'j', resizeFocusedWindow(function(w,s,f)
+      local step = winSizeMult * s:frame()._h
+      f.h = f.h + windowSizeStep
+      return f
+  end))
+
+  prefix.bind({'shift'}, 'k', resizeFocusedWindow(function(w,s,f)
+    local step = winSizeMult * s:frame()._h
+    f.y = f.y - step
+    f.h = f.h + step
+    return f
+  end))
+
+  prefix.bind({'shift'}, 'l', resizeFocusedWindow(function(w,s,f)
+      local step = winSizeMult * s:frame()._w
+      f.w = f.w + step
+      return f
+  end))
+
+end -- }}}
+
+------------------------------------------------------------------------
+--                            Helper Utils                            --
+------------------------------------------------------------------------
 function langSwitch(code) -- {{{
   return function()
-    if code == "EN" then
-      hs.keycodes.setLayout("U.S.")
-    elseif code == "RU" then
-      hs.keycodes.setLayout("Russian - Phonetic")
-    end
+    hs.keycodes.setLayout(code)
+    hs.notify.show("Landguage", "", "Switched to "..code)
   end
 end -- }}}
 
-function setFrame(w, f) -- {{{
+function winSetFrame(w, f) -- {{{
   w:setFrame(f, 0)
 end -- }}}
 
+function winSavePosition(w, notify) -- {{{
+  windowMap[w:id()] = w:frame()
+  print(notify)
+  if notify == true then
+    hs.notify.show("Window Position", "Success", "Window position saved...")
+  end
+end -- }}}
+
+function winLoadPosition(w, notify) -- {{{
+  rect = windowMap[w:id()]
+  if rect == nil then
+    print("Window lacks rect info")
+    if notify == true then
+      hs.notify.show("Window Position", "Error", "Window lacks rect info...")
+    end
+  else
+    winSetFrame(w, rect)
+  end
+end -- }}}
+
 function resizeFocusedWindow(fn)
-  local w = hs.window.focusedWindow()
-  setFrame(w, fn(w:frame()))
+  return function()
+    local w = hs.window.focusedWindow()
+    local s = w:screen()
+    winSetFrame(w, fn(w, s, w:frame()))
+  end
 end
 
-function bindGrid(key, grid, screens) -- {{{
+function bindGrid(pfx, key, grid, screens) -- {{{
   local uGrid = {}
-  for k,v in pairs(grid) do
+  for k, v in pairs(grid) do
     uGrid[k] = string.reverse(v)
   end
 
   local winSizeFun = function(rect, hint)
     local w = hs.window.focusedWindow()
-    windowMap[w:id()] = rect
-    setFrame(w, rect)
+    winSetFrame(w, rect)
+    winSavePosition(w, true)
   end
 
-  prefix.bind('', key, function() 
+  pfx.bind('', key, function() 
     if not(ggrid and ggrid.active == true) then
       ggrid = tmgrid.showGrid(grid, hs.screen.allScreens(), winSizeFun)
     end
   end)
 
-  prefix.bind('shift', key, function() 
+  pfx.bind('shift', key, function() 
     if not(ggrid and ggrid.active == true) then
       ggrid = tmgrid.showGrid(uGrid, hs.screen.allScreens(), winSizeFun)
     end
@@ -67,128 +187,118 @@ function winSelect() -- {{{
   end)
 end -- }}}
 
-function windowYank() -- {{{
-  local w = hs.window.focusedWindow()
 
-  windowYankBuffer = {}
-  windowYankBuffer['win_id'] = w:id()
-
-  print(windowYankBuffer['win_id'])
-end -- }}}
-
-function windowPut() -- {{{
-  if not(windowYankBuffer) then
-    print("Window Buffer not set")
-    return 
+function winSaveFrameAll()
+  for i, win in pairs(hs.window.allWindows()) do
+    winSavePosition(win, false)
   end
-  local win_id = windowYankBuffer['win_id']
-  print(windowYankBuffer['win_id'])
+end
 
-  local w = hs.window.find(win_id)
-  if not(w) then 
-    print("No window with id")
-    return
-  end
+function winSaveFrame()
+  winSavePosition(hs.window.focusedWindow(), true)
+end
 
-  local cur_screen = hs.screen.mainScreen()
-  local scs = hs.screen.allScreens()
+function winRestoreFrame()
+  winLoadPosition(hs.window.focusedWindow(), true)
+end
 
-  tmgrid.showGrid({'.'}, scs, function(rect, hint) 
-    local screen_num = tonumber(string.sub(hint, 0, 1))
-    w:moveToScreen(scs[screen_num])
-  end)
-end -- }}}
 ------------------------------------------------------------------------
---                           Configuration                            --
+--                              CHUNKWM                               --
 ------------------------------------------------------------------------
 
--- Global Mappings
+-- hyper_mod = {'ctrl', 'alt', 'cmd'}
+--
+-- function cwmap(mods, key, args)
+--   hs.hotkey.bind(mods, key, function()
+--     out, st, ty, rc = hs.execute('/usr/local/bin/chunkc ' .. args)
+--     print(st, out, args)
+--   end)
+-- end
+--
+-- cwmap(hyper_mod , 'h'     , 'tiling::window --focus west')
+-- cwmap(hyper_mod , 'left'  , 'tiling::window --focus west')
+--
+-- cwmap(hyper_mod , 'j'     , 'tiling::window --focus south')
+-- cwmap(hyper_mod , 'down'  , 'tiling::window --focus south')
+--
+-- cwmap(hyper_mod , 'k'     , 'tiling::window --focus north')
+-- cwmap(hyper_mod , 'up'    , 'tiling::window --focus north')
+--
+-- cwmap(hyper_mod , 'l'     , 'tiling::window --focus east')
+-- cwmap(hyper_mod , 'right' , 'tiling::window --focus east')
+--
+-- cwmap(hyper_mod , 'p' , 'tiling::window --toggle parent')
+-- cwmap(hyper_mod , 'f' , 'tiling::window --toggle fullscreen')
+--
+-- cwmap(hyper_mod , 'm' , 'set bsp_split_mode vertical')
+-- cwmap(hyper_mod , 'n' , 'set bsp_split_mode horizontal')
+--
+--
+-- cwmap({'alt'}, 'a', 'tiling::desktop --layout bsp')
+-- cwmap({'alt'}, 's', 'tiling::desktop --layout monocle')
+-- cwmap({'alt'}, 'd', 'tiling::desktop --layout float')
+--
+-- cwmap({'alt'}, 'q', 'tiling::desktop --toggle offset')
+--
+-- for i=1,9 do  -- -- -- -- -- -- -- -- -- --
+------------------------------------------------------------------------
+--   cwmap({'shift', 'alt'}, is, 'tiling::window --send-to-desktop ' .. is)
+-- end
+--
+-- cwmap({'shift', 'alt'}, 'space', 'tiling::window --toggle float')
 
-prefix.bind('', '`', hs.toggleConsole)
-prefix.bind('', '\\', hs.reload)
 
-prefix.bind('', 'y', windowYank)
-prefix.bind('', 'p', windowPut)
+------------------------------------------------------------------------
+--                               Unused                               --
+------------------------------------------------------------------------
 
-prefix.bind('shift', '=', function() 
-  local w = hs.window.focusedWindow()
-  w:close()
-end)
+-- function langSwitch(code) -- {{{
+--   return function()
+--     if code == "EN" then
+--       hs.keycodes.setLayout("U.S.")
+--     elseif code == "RU" then
+--       hs.keycodes.setLayout("Russian - Phonetic")
+--     end
+--   end
+-- end -- }}}
 
--- Language Manager
 
-prefix.bind({}, 'n', function() 
-  langSwitch("EN")()
-end)
+-- function windowYank() -- {{{
+--   local w = hs.window.focusedWindow()
+--
+--   windowYankBuffer = {}
+--   windowYankBuffer['win_id'] = w:id()
+--
+--   print(windowYankBuffer['win_id'])
+-- end -- }}}
+-- function windowPut() -- {{{
+--   if not(windowYankBuffer) then
+--     print("Window Buffer not set")
+--     return 
+--   end
+--   local win_id = windowYankBuffer['win_id']
+--   print(windowYankBuffer['win_id'])
+--
+--   local w = hs.window.find(win_id)
+--   if not(w) then 
+--     print("No window with id")
+--     return
+--   end
+--
+--   local cur_screen = hs.screen.mainScreen()
+--   local scs = hs.screen.allScreens()
+--
+--   tmgrid.showGrid({'.'}, scs, function(rect, hint) 
+--     local screen_num = tonumber(string.sub(hint, 0, 1))
+--     w:moveToScreen(scs[screen_num])
+--   end)
+-- end -- }}}
 
-prefix.bind({}, 'm', function() 
-  langSwitch("RU")()
-end)
-
--- Window Sizer
-
-prefix.bind({}, 'up', function() 
-  resizeFocusedWindow(function(f)
-    f.y = f.y - windowSizeStep
-    f.h = f.h + windowSizeStep
-    return f
-  end)
-end) 
-
-prefix.bind({}, 'down', function() 
-  resizeFocusedWindow(function(f)
-    f.h = f.h + windowSizeStep
-    return f
-  end)
-end) 
-
-prefix.bind({}, 'left', function() 
-  resizeFocusedWindow(function(f)
-    f.x = f.x - windowSizeStep
-    f.w = f.w + windowSizeStep
-    return f
-  end)
-end) 
-
-prefix.bind({}, 'right', function() 
-  resizeFocusedWindow(function(f)
-    f.w = f.w + windowSizeStep
-    return f
-  end)
-end) 
-
--- Window Manager
-
-bindGrid("q", {
-  "abc",
-  "abc",
-})
-
-bindGrid("w", {
-  "aaaabbbbbccc",
-})
-
-bindGrid("e", {
-  "bbbbbbaaaa",
-  "bbbbbbaaaa",
-  "bbbbbbcccc", 
-})
-
-bindGrid("r", {
-  "aaaaaabbbb",
-})
-
-bindGrid("t", {
-  "aaaaaabbbb",
-  "aaaaaabbbb",
-  "aaaaaabbbb",
-  "aaaaaacccc", 
-  "aaaaaacccc", 
-})
-
-bindGrid("y", {
-  "aaaaaabbbb",
-})
+-- bindGrid("g", {
+--   "aab",
+--   "aab",
+--   "aac"
+-- })
 
 -- hs.hotkey.bind({'ctrl', 'alt'}, "=", function() 
 --   local screens = hs.screen.allScreens()
@@ -212,30 +322,6 @@ bindGrid("y", {
 --     print("-------------")
 --   end
 -- end)
-
-hs.hotkey.bind({'ctrl', 'alt'}, "\\", function() 
-  winSelect()
-end)
-
-prefix.bind({'shift'}, "/", function() 
-  local w = hs.window.focusedWindow()
-  rect = windowMap[w:id()]
-  if rect == nil then
-    print("Window lacks rect info")
-  else
-    setFrame(w, rect)
-  end
-end)
-
--- bindGrid("g", {
---   "aab",
---   "aab",
---   "aac"
--- })
-
-------------------------------------------------------------------------
---                               Unused                               --
-------------------------------------------------------------------------
 
 -- qa.setMenu({
 --   qa.mkmenu("Languages", {
@@ -269,3 +355,12 @@ end)
 --   manager = "dsawyer@uber.com"
 --   utils.composeMail(manager, "Hello", "<b>Focus:</b> lalalal \n")
 -- end
+--
+--
+
+------------------------------------------------------------------------
+--                                INIT                                --
+------------------------------------------------------------------------
+
+init_keymap()
+
