1fc8c13c5dead6189e2090a281e0ea1f00b374e0
Oleg Utkin <oleg@nonlogical.io>
2019-10-28 21:03:35 -0700

    Update

--- common/zsh/zplugins.xlink/01-nld-base/02-paths.plugin.zsh
+++ common/zsh/zplugins.xlink/01-nld-base/02-paths.plugin.zsh
@@ -3,6 +3,11 @@ if [[ "$PLATFORM" == "LINUX" ]]; then
   path_prepend "$HOME/.linuxbrew/bin"
 fi
 
+# Setting up Python3
+if [[ "$PLATFORM" == "MAC" ]]; then
+  path_prepend "$HOME/Library/Python/3.7/bin"
+fi
+
 # Add local Bin dirs
 path_prepend "$HOME/.local/bin"
 path_prepend "$HOME/bin"
