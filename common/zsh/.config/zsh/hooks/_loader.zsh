9e016e69a50c1fb0512123cfc5c9ddfdf3041c4f
oleg.utkin <oleg.utkin@uber.com>
2025-12-19 20:38:13 +0000

    update: Fri Dec 19 08:38:13 PM UTC 2025 | outer-space

--- common/zsh/.config/zsh/hooks/_loader.zsh
+++ common/zsh/.config/zsh/hooks/_loader.zsh
@@ -5,18 +5,18 @@
 # Loads all .zsh files from a directory in numeric order
 # ------------------------------------------------------------------------------
 
-nl_hooks_load() {
+_hook_dir_load() {
     local hook_dir="$1"
-    
+
     if [[ -z "$hook_dir" ]]; then
         echo "Error: hl_hooks_load requires a directory path" >&2
         return 1
     fi
-    
+
     if [[ ! -d "$hook_dir" ]]; then
         return 0  # Directory doesn't exist, silently skip
     fi
-    
+
     # Load all .zsh files in the directory, sorted numerically
     # (N) - null glob: don't error if no matches
     # (on) - order numerically
@@ -27,3 +27,5 @@ nl_hooks_load() {
     done
 }
 
+_hook_dir_load "$HOOK_DIR"
+unset -f _hook_dir_load
---
+++ test.zsh
@@ -0,0 +1,1 @@
+echo "Hello $X_NAME"
