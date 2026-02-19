01b29d3d2a1b65899c5cd6a3963b2f988f1261d5
Oleg Utkin <oleg.utkin@uber.com>
2026-02-19 10:51:23 -0800

    Thu Feb 19 10:51:23 PST 2026 :: checkpoint

---
+++ common/nvim/.config/nvim.xlink/lua/plugins/tools__toggleterm.lua
@@ -0,0 +1,45 @@
+return {
+  "akinsho/toggleterm.nvim",
+  version = "*",
+  cmd = { "ToggleTerm", "TermExec" },
+  keys = {
+    { "<leader>tt", "<cmd>ToggleTerm direction=float<cr>", desc = "Toggle floating terminal" },
+    { "<leader>th", "<cmd>ToggleTerm direction=horizontal<cr>", desc = "Toggle horizontal terminal" },
+    { "<leader>tv", "<cmd>ToggleTerm direction=vertical<cr>", desc = "Toggle vertical terminal" },
+  },
+  lazy = false,
+  config = function()
+    require("toggleterm").setup({
+      size = function(term)
+        if term.direction == "horizontal" then
+          return 15
+        elseif term.direction == "vertical" then
+          return vim.o.columns * 0.5
+        end
+      end,
+      open_mapping = [[<c-=>]],
+      hide_numbers = true,
+      shade_terminals = true,
+      shading_factor = 2,
+      start_in_insert = true,
+      insert_mappings = true,
+      terminal_mappings = true,
+      persist_size = true,
+      persist_mode = true,
+      direction = "float",
+      close_on_exit = true,
+      shell = vim.o.shell,
+      auto_scroll = true,
+      float_opts = {
+        border = "curved",
+        width = function()
+          return math.floor(vim.o.columns * 0.8)
+        end,
+        height = function()
+          return math.floor(vim.o.lines * 0.8)
+        end,
+        winblend = 0,
+      },
+    })
+  end,
+}
--- common/nvim/.config/nvim.xlink/lua/plugins/ui__bufferline.lua
+++ common/nvim/.config/nvim.xlink/lua/plugins/ui__bufferline.lua
@@ -3,5 +3,33 @@ return {
   version = "*",
   event = "VeryLazy",
   dependencies = {'nvim-tree/nvim-web-devicons'},
-  opts = {},
+  opts = {
+    options = {
+      -- Use Unicode characters for status indicators
+      indicator = {
+        icon = '▎',
+        style = 'icon',
+      },
+      buffer_close_icon = '×',
+      modified_icon = '●',
+      close_icon = '×',
+      left_trunc_marker = '◀',
+      right_trunc_marker = '▶',
+
+      -- Separator style using Unicode
+      separator_style = "thin", -- Uses │ character
+
+      -- Keep devicons for file type icons
+      show_buffer_icons = true,
+      show_buffer_close_icons = true,
+      show_close_icon = true,
+
+      -- Other useful settings
+      diagnostics = "nvim_lsp",
+      diagnostics_indicator = function(count, level)
+        local icon = level:match("error") and "✖ " or "⚠ "
+        return " " .. icon .. count
+      end,
+    },
+  },
 }
